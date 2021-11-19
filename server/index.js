6d9df75f42e2eff41d2410b761cd8993c9a9e9f3
Michael Payne <mpaynei@umich.edu>
2021-11-18 21:08:49 -0500

    updating

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-// include and initialize the rollbar library with your access token
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// include and initialize the rollbar library with your access token
 let Rollbar = require("rollbar");
 let rollbar = new Rollbar({
   accessToken: "3120b794eb924901a98ec443f1a8396f",
@@ -20,13 +20,14 @@ let students = ["Sam", "Bradley", "Irene", "Michael"];
 
 app.use(express.json());
 app.use(cors());
-
 app.use(express.static("client"));
 // app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
+
+app.get("/main");
 
 app.get("/api/students", (req, res) => {
   rollbar.info("Someone got the list of students on page load");
